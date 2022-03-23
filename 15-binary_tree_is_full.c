ed0225a8c91e49cfd2b40f27d2a0cd9439ea287e
DaboRestrepo <daborestrepo@gmail.com>
2022-03-23 15:40:38 -0500

    Check if full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Check if the node is leaf.
+ * @node: pointer to a node to be check.
+ * Return: 1 if is leaf, 0 if not.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * binary_tree_is_full - Check if the tree is full.
+ * @tree: pointer to the node.
+ * Return: 0 or 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+	if (left == 0 || right == 0)
+		return (0);
+	return (1);
+}
