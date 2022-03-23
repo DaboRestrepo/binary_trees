2b50dd0af2f0bc5827ca37f827e3a308931ab9ef
DaboRestrepo <daborestrepo@gmail.com>
2022-03-22 19:19:38 -0500

    First 9 points

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - Insert a node in the right.
+ * @parent: pointer to the parent node.
+ * @value: node value.
+ * Return: the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Check if the node is a root.
+ * @node: node to be check.
+ * Return: 1 if is a root, 0 if not.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	else
+		return (1);
+}
